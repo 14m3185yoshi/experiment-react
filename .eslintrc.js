a23f76695e88df53c69f4f2edb41ab91be6e9a4b
14m3185yoshi <nytennis0713555555@gmail.com>
2021-12-19 22:19:12 +0900

    🔧 configuring eslint

--- .eslintrc.js
+++ .eslintrc.js
@@ -1,19 +1,39 @@
 module.exports = {
+    // NOTE: グローバルな変数を環境に応じて定義してくれるもの。
     "env": {
         "browser": true,
         "es2021": true
     },
-    "extends": "plugin:react/recommended",
+
+    // NOTE: 自動的にrulesにルールが追加されている。(rulesで上書きも可能。)
+    "extends": ["plugin:react/recommended"],
+
     "parserOptions": {
+        // NOTE: ES2015やJSX等の最新の構文を使えるようにするために。
         "ecmaFeatures": {
             "jsx": true
         },
-        "ecmaVersion": 13,
+        // NOTE: ECMAScriptのバージョンを指定。(正しい書き方は2021)
+        "ecmaVersion": 2021,
         "sourceType": "module"
     },
+
+    //NOTE: 外部のパッケージのルールを追加したい時に。(例えばrulesにreact/prop-typesなどのeslintにないものを設定できる)
     "plugins": [
         "react"
     ],
+
+    // NOTE: エラーの種類は3種類(error(0), warn(1), off(2))
     "rules": {
+        "semi": ["error", "always"],
+        "quotes": ["error", "double"], // NOTE: doubleがデフォルトなので、 「"quotes": "error"」でもOK
+        // "react/prop-types": off,
+    },
+
+    // NOTE: package.jsonに記載されているversionを見て、エラーを出すかなどを判断してくれる。
+    "settings": {
+        "react": {
+            "version": "detect"
+        }
     }
 };
